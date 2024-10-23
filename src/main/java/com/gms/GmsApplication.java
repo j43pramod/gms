bdfaa50cb46728800d993023cb57b0df04e5aedd
j43pramod <100414314+j43pramod@users.noreply.github.com>
2024-10-23 13:25:48 +0530

    Add files via upload
    First upload

---
+++ src/main/java/com/gms/GmsApplication.java
@@ -0,0 +1,47 @@
+package com.gms;
+
+import com.gms.auth.AuthenticationService;
+import com.gms.auth.RegisterRequest;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import static com.gms.user.Role.ADMIN;
+import static com.gms.user.Role.MANAGER;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class GmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmsApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service
+	) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("admin@mail.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+			var manager = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("manager@mail.com")
+					.password("password")
+					.role(MANAGER)
+					.build();
+			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+		};
+	}
+}
---
+++ src/main/java/com/gms/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.gms.token;
+
+public enum TokenType {
+  BEARER
+}
